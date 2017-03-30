c05aa98c088ca8a5a479dda456974a0116853c4d
Sid Ngeth <sngeth@gmail.com>
2017-03-29 21:39:40 -0400

    Solution 16 in Ruby

---
+++ 16.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+
+# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+# What is the sum of the digits of the number 2^1000?
+
+class ProjectEulerTests < Minitest::Test
+  def power_digit_sum
+    (2**1000).to_s.chars.map(&:to_i).reduce(:+)
+  end
+
+  def test_lattice_paths
+    assert 1366, power_digit_sum
+  end
+end
